5686fa7c0f38aad24259e7f4065e2e205b38c4c2
Surya <54676684+gpsurya@users.noreply.github.com>
2021-09-19 13:23:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
             <Header />
             <Landing />
             <About />
-            <Projects />
             <Contact />
             <Footer />
           </Route>
