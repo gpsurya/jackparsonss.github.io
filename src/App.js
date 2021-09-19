b23270f55dfd5eba7e0409886a15563323e14659
Surya <54676684+gpsurya@users.noreply.github.com>
2021-09-19 12:53:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from "./Components/NavBar/Header";
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "./Components/LandingPage/Landing";
-import Projects from "./Components/Projects Page/Projects";
 import About from "./Components/About Me Page/About";
 import Contact from "./Components/Contact Me Page/Contact";
 import Footer from "./Components/Footer/Footer";
